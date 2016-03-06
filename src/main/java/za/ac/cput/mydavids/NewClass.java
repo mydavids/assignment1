e758ea18592632f7230f8c0510cae38cbfdd7ae9
mydavids <yusiry.davids@gmail.com>
2016-03-06 19:55:26 +0200

    Commit modified file

--- src/main/java/za/ac/cput/mydavids/NewClass.java
+++ src/main/java/za/ac/cput/mydavids/NewClass.java
@@ -4,4 +4,10 @@ package za.ac.cput.mydavids;
  * Created by Yusiry Davids on 3/6/2016.
  */
 public class NewClass {
+
+    private int x = 5;
+
+    public int returnX(){
+        return x;
+    }
 }
